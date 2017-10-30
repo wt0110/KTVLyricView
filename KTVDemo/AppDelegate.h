98292b4b76cd134531952e18142c15489d484e7c
王涛 <wt0110@126.com>
2017-10-30 15:46:35 +0800

    Initial Commit

---
+++ KTVDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KTVDemo
+//
+//  Created by wangtao on 2017/10/30.
+//  Copyright © 2017年 wangtao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KTVDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  KTVDemo
+//
+//  Created by wangtao on 2017/10/30.
+//  Copyright © 2017年 wangtao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
